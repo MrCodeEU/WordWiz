e8fa21b07f62b2e9a509305647e4fd8bd6ccb31f
Michael Reinegger <reinemic2.0@gmail.com>
2024-12-10 20:06:38 +0100

    feat: implement tabs for known letters and unscramble sections; enhance layout and toast component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
 	import { onMount } from 'svelte';
 	import { i18n } from '$lib/i18n';
 	import { page } from '$app/stores';
+	import { Tabs, TabItem } from 'flowbite-svelte';
 	import logo from '$lib/images/Logo-h.png';
 	import * as m from '$lib/paraglide/messages.js';
 	import type { AvailableLanguageTag } from '$lib/paraglide/runtime';
@@ -43,7 +44,7 @@
 
 	$effect(() => {
 		if (showNoWordsMessage) {
-			trigger("No words found");
+			trigger('No words found');
 		}
 	});
 
@@ -80,7 +81,8 @@
 		const wordLengthsSet = new Set(wordLengths);
 		const wordLengthsArray = Array.from(wordLengthsSet);
 		wordLengthsMap = new Map(
-			wordLengthsArray.sort((a, b) => a - b)
+			wordLengthsArray
+				.sort((a, b) => a - b)
 				.map((length) => [length, results.filter((word) => word.length === length)])
 		);
 	}
@@ -131,52 +133,71 @@
 	<LangToggle />
 </Navbar>
 
-<Toast transition={blur} params={{ amount: 10 }} position="top-right" bind:toastStatus class="mr-5 mt-16" dismissable={false}>
+<Toast
+	transition={blur}
+	params={{ amount: 10 }}
+	position="top-right"
+	bind:toastStatus
+	class="mr-5 mt-16"
+	dismissable={false}
+>
 	<InfoCircleOutline
 		slot="icon"
-		class="text-primary-500 bg-primary-100 dark:bg-primary-800 dark:text-primary-200 h-6 w-6"
+		class="h-6 w-6 bg-primary-100 text-primary-500 dark:bg-primary-800 dark:text-primary-200"
 	/>
 	{toastMessage}
 </Toast>
 
-<div class="flex justify-center pt-20">
-	<Card id="input" size="lg">
-		<h2 class="m-auto mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-			{m.cardHeader()}
-		</h2>
-		<Label for="large-input" class="mb-2 block">{m.search()}</Label>
-		<Input
-			id="default-input"
-			size="lg"
-			placeholder="b,n,h,g"
-			type="text"
-			bind:value={letters}
-			oninput={addCommas}
-			class="mb-5"
-		/>
-		<div class="flex w-full flex-row justify-between">
-			<Button onclick={findWords} class="w-1/3">Find Words</Button>
-			{#await maxLength}
-				<Spinner size="10" />
-			{:then maxLength}
-				<p class="text-gray-500 dark:text-gray-400">Max word length: {maxLength}</p>
-			{:catch error}
-				<p class="text-red-500 dark:text-red-400">Error: {error.message}</p>
-				
-			{/await}
-			<ButtonGroup>
-				<RadioButton value="de" bind:group={selectedLang}
-					><FlagOutline class="h-7 w-7" />Deutsch</RadioButton
-				>
-				<RadioButton value="en" bind:group={selectedLang}
-					><FlagOutline class="h-7 w-7" />English</RadioButton
-				>
-			</ButtonGroup>
-		</div>
-	</Card>
+<div class="w-1/2 ml-auto mr-auto pt-20">
+	<Tabs>
+		<TabItem open>
+			<span slot="title">Known Letters</span>
+			<Card id="input" size="lg">
+				<h2 class="m-auto mb-4 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+					{m.cardHeader()}
+				</h2>
+				<Label for="large-input" class="mb-2 block">{m.search()}</Label>
+				<Input
+					id="default-input"
+					size="lg"
+					placeholder="b,n,h,g"
+					type="text"
+					bind:value={letters}
+					oninput={addCommas}
+					class="mb-5"
+				/>
+				<div class="flex w-full flex-row justify-between">
+					<Button onclick={findWords} class="w-1/3">Find Words</Button>
+					{#await maxLength}
+						<Spinner size="10" />
+					{:then maxLength}
+						<p class="text-gray-500 dark:text-gray-400">Max word length: {maxLength}</p>
+					{:catch error}
+						<p class="text-red-500 dark:text-red-400">Error: {error.message}</p>
+					{/await}
+					<ButtonGroup>
+						<RadioButton value="de" bind:group={selectedLang}
+							><FlagOutline class="h-7 w-7" />Deutsch</RadioButton
+						>
+						<RadioButton value="en" bind:group={selectedLang}
+							><FlagOutline class="h-7 w-7" />English</RadioButton
+						>
+					</ButtonGroup>
+				</div>
+			</Card>
+		</TabItem>
+		<TabItem>
+			<span slot="title">Unscramble</span>
+			<p class="text-sm text-gray-500 dark:text-gray-400">
+				<b>Settings:</b>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+				labore et dolore magna aliqua.
+			</p>
+		</TabItem>
+	</Tabs>
 </div>
 
-<div class="width-screen flex justify-center pt-10">
+<div class="width-screen flex flex-col justify-center pt-10">
 	<div class="card w1/2">
 		{#if loading}
 			<div class="flex w-screen justify-center">
@@ -188,14 +209,16 @@
 			<div class="flex w-screen justify-center">
 				<div class="flex w-3/4 flex-wrap pt-16">
 					{#each wordLengthsMap as group}
-						<div class="flex flex-col w-full">
-							<h3 class="text-lg font-bold pt-5">{group[0]} letter words</h3>
+						<div class="flex w-full flex-col">
+							<h3 class="pt-5 text-lg font-bold">{group[0]} letter words</h3>
 							<div class="flex flex-row flex-wrap justify-items-center">
 								{#each group[1] as word}
-								<div class="px-2 py-1 mx-2 my-1 type-scale4 rounded-md bg-orange-400 text-center w-min">
-									{word}
-								</div>
-							{/each}
+									<div
+										class="type-scale4 mx-2 my-1 w-min rounded-md bg-orange-400 px-2 py-1 text-center"
+									>
+										{word}
+									</div>
+								{/each}
 							</div>
 						</div>
 					{/each}
